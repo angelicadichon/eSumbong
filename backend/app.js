e3e3cacabf5a49599248a41528c17c48897e318d
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-10-25 18:09:48 +0800

    chnage app

--- backend/app.js
+++ backend/app.js
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
-import userRoutes from "./routes/userRoutes.js";
+import userRoutes from "./routes/authRoutes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
