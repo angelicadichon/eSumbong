e87d6ed30aa67a8c15ff015d8701e30efcf5c963
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-10-25 18:51:54 +0800

    fix regex error

--- backend/app.js
+++ backend/app.js
@@ -21,9 +21,10 @@ app.use("/api/users", userRoutes);
 
 // Serve frontend
 app.use(express.static(path.join(__dirname, "frontend")));
-app.get("*", (req, res) =>
-  res.sendFile(path.join(__dirname, "frontend", "index.html"))
-);
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend", "index.html"));
+});
+
 
 // Start server
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
