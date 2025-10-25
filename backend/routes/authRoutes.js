78b3326766523e3ad833f1ebf36f02c4a60545c0
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-10-25 19:17:48 +0800

    fixing index.html

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { index, login, getUser } from "../controllers/authController.js";
+import { register, login, getUser } from "../controllers/authController.js";
 
 const router = express.Router();
 
-router.post("/index", index);
+router.post("/register", register);
 router.post("/login", login);
 router.get("/user", getUser);
 
