85de2b27689d437ad0f9e7a75afa28fb2242833c
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-10-27 17:15:02 +0800

    fix server 3

--- backend/server.js
+++ backend/server.js
@@ -101,7 +101,7 @@ app.post("/api/login", async (req, res) => {
 const frontendPath = path.join(__dirname, "frontend");
 app.use(express.static(frontendPath));
 
-app.get("/*", (req, res) => {
+app.get("*", (req, res) => {
   res.sendFile(path.join(frontendPath, "index.html"));
 });
 
