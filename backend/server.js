ef0e2929cba95073387d022fbf7f5d1697140102
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-10-31 17:10:58 +0800

    check link

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5200;
 
 // Directory setup
 const __filename = fileURLToPath(import.meta.url);
