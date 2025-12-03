b96ff6777f5a2795ef15f0d2b63ee489d7c3602a
Ma. Angelica S. Dichon <dichonmaangelica7@gmail.com>
2025-12-03 17:54:52 +0800

    fix icons

--- backend/frontend/js/sk-profile.js
+++ backend/frontend/js/sk-profile.js
@@ -411,16 +411,3 @@ document.addEventListener('DOMContentLoaded', function() {
     setupInputValidation();
 });
 
-function logout() {
-    const confirmed = window.confirm("Are you sure you want to logout?");
-    
-    if (confirmed) {
-        localStorage.removeItem('username');
-        localStorage.removeItem('role');
-        
-        localStorage.removeItem('token');
-        localStorage.removeItem('userData');
-        
-        window.location.href = 'index.html';
-    }
-}
